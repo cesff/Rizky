13c592ccb2c17354f55d2270cfb3ac3ce7e1bc23
cesff <rizkyepul3.com>
2019-11-21 13:59:03 +0700

    tambah file tambah

---
+++ tambah.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>CRUD</title>
+</head>
+
+<body>
+    <center><b>Silahkan Isi Biodata!</b></center>
+
+    <form action="proses.php?aksi=tambah" method="post">
+
+        <label><b>Nama</b></label>
+        <input type="text" name="nama" class="form-control" placeholder="Nama....." required>
+        <br>
+        <br>
+        <label><b>Tanggal Lahir</b></label>
+        <input type="date" name="tanggal_lahir">
+        <br>
+        <br>
+        <button type submit="submit" name="simpan">
+
+    </form>
+    </body>
+    </html>    
